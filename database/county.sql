acd71d95aa6996a6ea5c7564a9e783bd1a0d08f5
MunkushiR <naserianr91@gmail.com>
2024-08-12 19:57:41 +0300

    done

--- database/county.sql
+++ database/county.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2024 at 06:46 PM
+-- Generation Time: Aug 12, 2024 at 06:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
